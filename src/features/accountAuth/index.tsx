88abe9da5914b9ceeb8c6eedfbe8620f07e296a5
rozhko_y <rc.drift18@gmail.com>
2023-05-07 17:32:45 +0200

    fixed public api in accountAuth and  playGround

--- src/features/accountAuth/index.tsx
+++ src/features/accountAuth/index.tsx
@@ -1,3 +1,4 @@
 export { SignInWith } from './ui/signInWith';
+export { SignUpForm } from './ui/signUpForm/index';
 export { AccountManagement } from './ui/accountManagement';
 export { SignInForm } from './ui/signInForm/lib';
