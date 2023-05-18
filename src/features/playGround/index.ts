988531964484373a4c23672e78d913f9ae5513dc
rozhko_y <45626144+rozhkoy@users.noreply.github.com>
2023-05-18 19:50:00 +0300

    Revert "Revert "rename useFindWinner to usePlayFieldHander, add markCell function in …""

--- src/features/playGround/index.ts
+++ src/features/playGround/index.ts
@@ -1,4 +1,4 @@
 export { GameInfo } from './ui/gameInfo/index';
 export { PlayField } from './ui/playField/index';
-export { useFindWinner } from './lib/useFindWinner';
+export { usePlayFieldHandler } from './lib/usePlayFieldHandler';
 export { GameBoardWrap } from './ui/gameBoardWrap/index';
