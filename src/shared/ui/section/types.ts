27acd6b2d188e17a9e5b1d6b160cf40c6e0422e1
George <54411613+e6voe9@users.noreply.github.com>
2023-06-08 21:38:57 +0200

    new section component

---
+++ src/shared/ui/section/types.ts
@@ -0,0 +1,6 @@
+import { PropsWithClassNameAndChildren } from '@/shared/types/propsWithClassNameAndChildren';
+
+export interface SectionProps extends PropsWithClassNameAndChildren {
+	className?: string;
+	title: string;
+}
