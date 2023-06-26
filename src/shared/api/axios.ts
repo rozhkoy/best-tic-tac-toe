79434db026b616d630f5b751d0593ecf80ca3d95
rozhko_y <rc.drift18@gmail.com>
2023-06-27 00:57:48 +0200

    move firebase config to shared layer and automatic addition of id token to hedaer

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,12 +1,27 @@
 import axios from 'axios';
-import { error } from 'console';
+import { Console, error } from 'console';
+import { config } from 'process';
+import { auth } from '../lib/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { resolve } from 'path';
+import { rejects } from 'assert';
 
 export const server = axios.create({
 	withCredentials: true,
 	baseURL: 'http://localhost:5000',
 });
 
-axios.interceptors.response.use(
+const userIdToken = new Promise((resolve, rejects) => {
+	onAuthStateChanged(auth, async (user) => {
+		if (user) {
+			resolve(await user.getIdToken());
+		} else {
+			rejects();
+		}
+	});
+});
+
+server.interceptors.response.use(
 	(response) => response,
 	(error) => {
 		if (error.response.status !== 200) {
@@ -14,3 +29,11 @@ axios.interceptors.response.use(
 		}
 	}
 );
+
+server.interceptors.request.use(async (config) => {
+	const idToken = await userIdToken;
+	if (config.headers && idToken) {
+		config.headers.Authorization = String(idToken);
+	}
+	return config;
+});
